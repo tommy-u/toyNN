69257f0454ebbb591a23725a570fb7dbe066be2c
Tommy Unger <tommyu@bu.edu>
2015-08-16 11:42:02 -0400

    new mem allocation bug found

--- src/tnn.c
+++ src/tnn.c
@@ -13,10 +13,10 @@ double tnn_rand_norm(const double mean, const double std)
 {
   double u, v, s;
   do{
-      u = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0;
-      v = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0;
-      s = u * u + v * v;
-    }
+    u = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0;
+    v = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0;
+    s = u * u + v * v;
+  }
   while( (s >= 1.0) || (s == 0.0) );
 
   s = sqrt(-2.0 * log(s) / s);
@@ -43,124 +43,122 @@ typedef struct {
   /* List of num_layers -1 many 2d rectangular arrays of connections
      between layer l and l-1. An inner loop over one of these 2d arrays
      iterates over the neurons in layer l-1. */
-  double ***connections;
+     double ***connections;
 
-} Net;
+   } Net;
 
-double * cost_derivative(Net *n, double *labels, double *output){
+   void tnn_cost_derivative(Net *n, double *labels, double *err){
   /* Partial derivatives for output activation using squared 
-    difference cost function */
-  double *derivs = calloc(n->layers[n->num_layers-1], sizeof(double));
-  uint i;
-  for (i = 0; i < n->layers[n->num_layers-1]; i++){
-    derivs[i] = output[i] - labels[i];
+     difference cost function */
+    uint i;
+    for (i = 0; i < n->layers[n->num_layers-1]; i++){
+      err[i] = n->output[i] - labels[i];
+    }
   }
-  return derivs;
-}
 
-void tnn_init_connections(Net *n){
-  
-  uint i;
-  for(i=0;i<n->num_layers-1;i++){
-    uint j;
-    for(j=0;j<n->layers[i+1];j++){
-      uint k;
-      for(k=0;k<n->layers[i];k++){
-        n->connections[i][j][k] = tnn_rand_norm(0,1);
+  void tnn_init_connections(Net *n){
+
+    uint i;
+    for(i=0;i<n->num_layers-1;i++){
+      uint j;
+      for(j=0;j<n->layers[i+1];j++){
+        uint k;
+        for(k=0;k<n->layers[i];k++){
+          n->connections[i][j][k] = tnn_rand_norm(0,1);
+        }
       }
     }
   }
-}
 
-void tnn_init_biases(Net *n){
-  uint i;
-  for(i=0;i<n->num_layers-1;i++){
-    uint j;
-    for(j=0;j<n->layers[i+1];j++){
-      n->biases[i][j] = tnn_rand_norm(0,1);
+  void tnn_init_biases(Net *n){
+    uint i;
+    for(i=0;i<n->num_layers-1;i++){
+      uint j;
+      for(j=0;j<n->layers[i+1];j++){
+        n->biases[i][j] = tnn_rand_norm(0,1);
+      }
     }
   }
-}
 
-Net * tnn_init_net(uint num_layers, uint *layers){
+  Net * tnn_init_net(uint num_layers, uint *layers){
   /* Creates Net struct. Assumes fully connected. */
-  uint i;
-  double *output;
-  double **biases, **pre_activations;
-  double ***connections;
+    uint i;
+    double *output;
+    double **biases, **pre_activations;
+    double ***connections;
 
   /* Allocate output array. */
-  output = malloc( layers[num_layers-1] * sizeof(double) );
+    output = malloc( layers[num_layers-1] * sizeof(double) );
 
   /* Allocate biases array. */
-  biases = malloc( num_layers * sizeof(double *) );
-  if(biases == NULL){
-    printf("allocation of biases in init_net failed \n");
-    exit(1);
-  }  
-  
-  /* Skip input layer. */
-  for(i=0; i<num_layers-1; i++){
-    biases[i] = malloc( layers[i+1] * sizeof(double) );
-    if(biases[i] == NULL){
-      printf("allocation of biases[%u] in init_net failed \n",i);
+    biases = malloc( num_layers * sizeof(double *) );
+    if(biases == NULL){
+      printf("allocation of biases in init_net failed \n");
       exit(1);
+    }  
+
+  /* Skip input layer. */
+    for(i=0; i<num_layers-1; i++){
+      biases[i] = malloc( layers[i+1] * sizeof(double) );
+      if(biases[i] == NULL){
+        printf("allocation of biases[%u] in init_net failed \n",i);
+        exit(1);
+      }
     }
-  }
 
   /* Allocate pre_activations array */
 
-  pre_activations = malloc( num_layers * sizeof(double*));
-  if (pre_activations == NULL){
-    printf("allocation of pre_activations in init_net failed \n");
-    exit(1);
-  } 
-
-  /* Skip input layer. */
-  for (i = 0; i < num_layers-1; i++){
-    pre_activations[i] = malloc (layers[i+1] * sizeof(double));
-    if (pre_activations[i] == NULL){
+    pre_activations = malloc( num_layers * sizeof(double*));
+    if (pre_activations == NULL){
       printf("allocation of pre_activations in init_net failed \n");
       exit(1);
     } 
-  }
+
+  /* Skip input layer. */
+    for (i = 0; i < num_layers-1; i++){
+      pre_activations[i] = malloc (layers[i+1] * sizeof(double));
+      if (pre_activations[i] == NULL){
+        printf("allocation of pre_activations in init_net failed \n");
+        exit(1);
+      } 
+    }
 
   /* Allocate connections. */
-  connections = malloc( (num_layers - 1 ) * sizeof(double * ) );
-  if(connections == NULL){
-    printf("allocation of connections in init_net failed \n");
-    exit(1);
-  }
-
-  /* Each connection matrix. */
-  for(i=0; i<num_layers - 1; i++){
-    connections[i] = malloc( layers[i+1] * sizeof(double * ) );
-    if(biases[i] == NULL){
-      printf("allocation of connections[%u] in init_net failed \n",i);
+    connections = malloc( (num_layers - 1 ) * sizeof(double * ) );
+    if(connections == NULL){
+      printf("allocation of connections in init_net failed \n");
       exit(1);
     }
-    /* Each neuron in the i+1th layer. */
-    uint j;
-    for(j=0; j<layers[i+1]; j++){
-      connections[i][j] = malloc ( layers[i] * sizeof(double) );
-      if(connections[i][j] == 0){
-	printf("allocation of connections[%u][%u] in init_net failed \n",i,j);
-	exit(1);
+
+  /* Each connection matrix. */
+    for(i=0; i<num_layers - 1; i++){
+      connections[i] = malloc( layers[i+1] * sizeof(double * ) );
+      if(biases[i] == NULL){
+        printf("allocation of connections[%u] in init_net failed \n",i);
+        exit(1);
       }
-    }
-  }
+    /* Each neuron in the i+1th layer. */
+      uint j;
+      for(j=0; j<layers[i+1]; j++){
+        connections[i][j] = malloc ( layers[i] * sizeof(double) );
+        if(connections[i][j] == 0){
+         printf("allocation of connections[%u][%u] in init_net failed \n",i,j);
+         exit(1);
+       }
+     }
+   }
 
   /* Create, initialize Net. */
-  Net *n = malloc( sizeof(Net) );
-  n->num_layers = num_layers;
-  n->layers = layers;
-  n->output = output;
-  n->biases = biases;
-  n->pre_activations = pre_activations;
-  n->connections = connections;
-  
+   Net *n = malloc( sizeof(Net) );
+   n->num_layers = num_layers;
+   n->layers = layers;
+   n->output = output;
+   n->biases = biases;
+   n->pre_activations = pre_activations;
+   n->connections = connections;
+
   /* Warm up prg */
-  for(i=0;i<10;i++)
+   for(i=0;i<10;i++)
     rand();
   tnn_init_connections(n);
   tnn_init_biases(n);
@@ -201,7 +199,7 @@ void tnn_destroy_net(Net *n){
 double tnn_sigmoid(double z){
   /* Sigmoid Fn */
   return 1 / ( 1 + exp( -1 * z ) );
- }
+}
 
 double tnn_sigmoid_prime(double z){
   /* Derivative of Sigmoid */
@@ -213,29 +211,29 @@ void tnn_feedforward(Net *n, double *input, uint train){
      k iterates over the input.  
      Train is a flag specifying if the pre_activations should
      be recorded. */
-  double *previous, *current;
-  previous = input;
-  
-  uint i;
+     double *previous, *current;
+     previous = input;
+
+     uint i;
   /* Loop over connection matrices between layers. */
-  for (i=0; i<n->num_layers-1; i++) {
+     for (i=0; i<n->num_layers-1; i++) {
     /* This will contain the activations. */
-    current = calloc(n->layers[i+1], sizeof(double) );
+      current = calloc(n->layers[i+1], sizeof(double) );
     /* Temporary storage for layer activation. */
-    uint j;
+      uint j;
 
     /* Loop over output neurons (of this layer). */
-    for(j=0; j<n->layers[i+1]; j++){
-      uint k;
+      for(j=0; j<n->layers[i+1]; j++){
+        uint k;
       /* Loop over incoming connection signals. This is the dot 
 	 product of the input vector and the weight matrix. */
-      for(k=0; k<n->layers[i]; k++){
-	current[j] += previous[k] * n->connections[i][j][k];
-      }
+        for(k=0; k<n->layers[i]; k++){
+         current[j] += previous[k] * n->connections[i][j][k];
+       }
       /* Add the bias term, this is the preactivation.  */
-      current[j] += n->biases[i][j];
-      
-      if(train)
+       current[j] += n->biases[i][j];
+
+       if(train)
         n->pre_activations[i][j] = current[j];
 
       /* This is the activation of the n->layers[i+1] neurons in
@@ -255,10 +253,10 @@ void tnn_feedforward(Net *n, double *input, uint train){
   }
   free(current);
   
-  }
+}
 
 void tnn_print_net(Net *n){
-  
+
   printf("----------------------------------------------\n");
   
   printf("layer counts in->out: ");
@@ -294,12 +292,12 @@ void tnn_print_net(Net *n){
     for(j=0;j<n->layers[i+1];j++){
       uint k=0;
       for(k=0;k<n->layers[i];k++){
-	printf("o: %u i: %u\t%.3f\n",j,k,n->connections[i][j][k]);
-      }
-    }
-  }
+       printf("o: %u i: %u\t%.3f\n",j,k,n->connections[i][j][k]);
+     }
+   }
+ }
 
-  printf("----------------------------------------------\n");
+ printf("----------------------------------------------\n");
 }
 
 
@@ -308,7 +306,7 @@ void tnn_print_output_activation(Net *n, double *in){
   printf("out vec: \n");
   uint i;
   for(i=0;i<n->layers[n->num_layers-1]; i++)
-    printf("%.3f \n",n->output[i]);
+    printf("n:%u v:%.3f \n",i,n->output[i]);
 }
 
 void tnn_print_pre_activations(Net *n){
@@ -324,27 +322,99 @@ void tnn_print_pre_activations(Net *n){
     for (j = 0; j < n->layers[i+1]; j++){
       printf("l:%u n:%u v: %.3f\n",i,j,n->pre_activations[i][j]);
     }
-    
   }
 }
 
-void tnn_backprop(){
+void tnn_allocate_error_arrays(Net *n, double **err){
+  err = malloc(n->num_layers-1 * sizeof(double*));
+  if(err==NULL){
+    printf("Failed to allocate error array in tnn_allocate_error_arrays\n");
+    exit(1);
+  }
+  uint i;
+  for(i=0; i<n->num_layers-1; i++){
+    printf("allocating err[%u]\n",i);
+    err[i] = calloc(n->layers[i], sizeof(double));
+    if(err[i]==NULL){
+      printf("Failed to allocate error[%u] array in tnn_allocate_error_arrays\n",i);
+      exit(1);
+    }
+  }
+}
+
+void tnn_generate_error(Net *n, double *labels, double **err){
+  /* Start at output layer, work way back toward input. */
+  uint i;  
+  for(i=n->num_layers-1; i>1; i--){
+    uint j;
+    /* Calculate error for each node. */
+    for(j=0;j<n->layers[i]; j++){
+      //printf("l:%u n:%u %f pre_activations\n",i,j,n->pre_activations[i-1][j]);
+//      printf("error: %f \n",err[j] * tnn_sigmoid_prime(n->pre_activations[i-1][j]));
+//      uint k;
+  //    for(k=0; k<n->layers[i-1]; k++){
+
+    //  }
+    }
+  }
+}
+
+void tnn_backprop(Net *n, double *in, double *labels){
+  /* Allocate error arrays. */
+  double **err = NULL;
+  tnn_allocate_error_arrays(n,err);
+
+  /* Acquires output vec as well as pre_activations. */
+  tnn_feedforward(n,in,1);
+  /* Compute error at each node, chain rule. */
+  //tnn_generate_error(n,labels, err);
+
+  printf("here\n");  
+  uint i;
+  for(i=0; i<n->num_layers-1; i++){
+    printf("free err[%u]\n",i);
+     free(err[i]);
+  }
+
+  free(err);
+
 
 }
+
+
+
 
 
 int main(){
   srand(time(NULL));
-  uint a[3]= {2, 3, 1};
-  double in[2] = {1, 1};
+  uint a[3]= {2, 2, 2};
   Net *n = tnn_init_net(3, a); 
 
-  tnn_feedforward(n, in, 1);
+  //Let's learn the copy function
+  printf("before training \n");
 
-  tnn_print_pre_activations(n);
+  double in[2] = {0, 0};
+  printf("0 0\n");  
+  tnn_print_output_activation(n,in);
+  
+  in[0] = 1;
+  printf("0 1\n");
+  tnn_print_output_activation(n,in);  
 
+  in[0] = 0;
+  in[1] = 1;
+  printf("1 0\n");
   tnn_print_output_activation(n,in);
 
+  in[0] = 1;
+  printf("1 1\n");
+  tnn_print_output_activation(n,in);
+
+  double label[2] = {0, 0};
+  in[0]=0; in[1]=0;
+  tnn_backprop(n, in, label);
+  tnn_print_output_activation(n,in);
+  
   tnn_destroy_net(n);
   printf("exiting gracefully \n");
   return 0;
